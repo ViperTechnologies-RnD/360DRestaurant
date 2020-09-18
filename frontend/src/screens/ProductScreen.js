979c2c8e8686e61c0decf045bbb69398a837a678
Sobhan Bahrami <sobhan.bahrami@metu.edu.tr>
2020-09-18 13:42:07 +0300

    fix key error - qty options
    quantity fetching in frontend needs a key attrib for option tag

--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -57,7 +57,7 @@ function ProductScreen (props) {
                     <li>
                         Qty: <select value={qty} onChange={(e) => {setQty(e.target.value)}}>
                             {[...Array(product.countInStock).keys()].map(x=> 
-                            <option value={x+1}>{x+1}</option>)}
+                            <option key={x+1} value={x+1}>{x+1}</option>)}
                         </select>
                     </li>
                     <li>
